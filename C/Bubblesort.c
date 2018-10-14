dc08724e116de5ccadfd422fff7d50380b75a809
Akhil789 <akhilk152@gmail.com>
2018-10-14 23:39:47 +0530

    Added Bubblesort
    Closes https://github.com/naveentata/Sortings-in-different-Languages/issues

---
+++ C/Bubblesort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+	int i=0,n;
+	int a[100];
+	printf("Enter the no of elements: ");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	bubblesort(a,n);
+	void bubblesort(int a[],int n)
+	{
+		int swap;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					swap=a[j];
+					a[j]=a[j+1];
+					a[j+1]=swap;
+				}
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		printf("After Sorting");
+		printf("%d",&a[i]);
+	}
+}
