0fef7e494fd858646ad087eacd88ca20b9e72ae3
vidyadeepa <44111486+vidyadeepa@users.noreply.github.com>
2018-10-14 22:45:48 +0530

    Add files via upload

---
+++ bubblesort.py
@@ -0,0 +1,24 @@
+a=int(input("no of elements: "))
+arr=[]
+for i in range(a):
+	b=int(input("elemts: "))
+	arr.append(b)
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n): 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+bubbleSort(arr) 
+  
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i]),  
