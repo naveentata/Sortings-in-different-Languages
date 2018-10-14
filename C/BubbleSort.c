f424813afd82619023271a949da51e71f16eca55
Salah Eddine Bachir-Cherif <gs_bachircherif@esi.dz>
2018-10-14 17:55:53 +0100

    Added printArray

--- C/BubbleSort.c
+++ C/BubbleSort.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-void BubbleSort(int (*arr)[], int length)
+void BubbleSort(int (*arr)[], int size)
 {
     int i, swap = 1;
     while (swap)
     {
         swap = 0;
-        for (i = 0; i < length - 1; i++)
+        for (i = 0; i < size - 1; i++)
         {
             if ((*arr)[i] > (*arr)[i + 1])
             {
@@ -17,15 +17,19 @@ void BubbleSort(int (*arr)[], int length)
         }
     }
 }
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("n");
+}
+
 int main()
 {
     int arr[5] = {85, 22, 98, 36, 2};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     BubbleSort(arr, n);
-    int i;
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d\n", arr[i]);
-    }
+    printArray(arr, n);
     return 0;
 }
