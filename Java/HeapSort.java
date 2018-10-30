5a30d0daec49577f95225b9826af5e241535cda2
Benaskeur Moussaab <gm_benaskeur@esi.dz>
2018-10-30 11:39:19 +0100

    Update Java/HeapSort.java

--- Java/HeapSort.java
+++ Java/HeapSort.java
@@ -1,75 +1,52 @@
+import java.util.Arrays;
+public class Heapsort {
 
-// Java program for implementation of Heap Sort 
-public class HeapSort 
-{ 
-    public void sort(int arr[]) 
-    { 
-        int n = arr.length; 
-  
-        // Build heap (rearrange array) 
-        for (int i = n / 2 - 1; i >= 0; i--) 
-            heapify(arr, n, i); 
-  
-        // One by one extract an element from heap 
-        for (int i=n-1; i>=0; i--) 
-        { 
-            // Move current root to end 
-            int temp = arr[0]; 
-            arr[0] = arr[i]; 
-            arr[i] = temp; 
-  
-            // call max heapify on the reduced heap 
-            heapify(arr, i, 0); 
-        } 
-    } 
-  
-    // To heapify a subtree rooted with node i which is 
-    // an index in arr[]. n is size of heap 
-    void heapify(int arr[], int n, int i) 
-    { 
-        int largest = i; // Initialize largest as root 
-        int l = 2*i + 1; // left = 2*i + 1 
-        int r = 2*i + 2; // right = 2*i + 2 
-  
-        // If left child is larger than root 
-        if (l < n && arr[l] > arr[largest]) 
-            largest = l; 
-  
-        // If right child is larger than largest so far 
-        if (r < n && arr[r] > arr[largest]) 
-            largest = r; 
-  
-        // If largest is not root 
-        if (largest != i) 
-        { 
-            int swap = arr[i]; 
-            arr[i] = arr[largest]; 
-            arr[largest] = swap; 
-  
-            // Recursively heapify the affected sub-tree 
-            heapify(arr, n, largest); 
-        } 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
-  
-    // Driver program 
-    public static void main(String args[]) 
-    { 
-        int arr[] = {12, 11, 13, 5, 6, 7}; 
-        int n = arr.length; 
-  
-        HeapSort ob = new HeapSort(); 
-        ob.sort(arr); 
-  
-        System.out.println("Sorted array is"); 
-        printArray(arr); 
-    } 
-} 
+	public static void main(String[] args) {
+		//test array
+		Integer[] arr = {1, 4, 3, 2, 64, 3, 2, 4, 5, 5, 2, 12, 14, 5, 3, 0, -1};
+		String[] strarr = {"hope you find this helpful!", "wef", "rg", "q2rq2r", "avs", "erhijer0g", "ewofij", "gwe", "q", "random"};
+		arr = heapsort(arr);
+		strarr = heapsort(strarr);
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(strarr));
+	}
+	
+	//O(nlogn) TIME, O(1) SPACE, NOT STABLE
+	public static <E extends Comparable<E>> E[] heapsort(E[] arr){
+		int heaplength = arr.length;
+		for(int i = arr.length/2; i>0;i--){
+			arr = maxheapify(arr, i, heaplength);
+		}
+		
+		for(int i=arr.length-1;i>=0;i--){
+			E max = arr[0];
+			arr[0] = arr[i];
+			arr[i] = max;
+			heaplength--;
+			arr = maxheapify(arr, 1, heaplength);
+		}
+		
+		return arr;
+	}
+	
+	//Creates maxheap from array
+	public static <E extends Comparable<E>> E[] maxheapify(E[] arr, Integer node, Integer heaplength){
+		Integer left = node*2;
+		Integer right = node*2+1;
+		Integer largest = node;
+		
+		if(left.compareTo(heaplength) <=0 && arr[left-1].compareTo(arr[node-1]) >= 0){
+			largest = left;
+		}
+		if(right.compareTo(heaplength) <= 0 && arr[right-1].compareTo(arr[largest-1]) >= 0){
+			largest = right;
+		}	
+		if(largest != node){
+			E temp = arr[node-1];
+			arr[node-1] = arr[largest-1];
+			arr[largest-1] = temp;
+			maxheapify(arr, largest, heaplength);
+		}
+		return arr;
+	}
+}
